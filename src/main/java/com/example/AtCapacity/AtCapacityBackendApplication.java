bef5a48cddcdcddee9a85b64441aa538f8a7435d
peng-0110 <joshuapeng2005@gmail.com>
2025-03-23 10:23:28 -0700

    add config

--- src/main/java/com/example/AtCapacity/AtCapacityBackendApplication.java
+++ src/main/java/com/example/AtCapacity/AtCapacityBackendApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @RestController
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class AtCapacityBackendApplication {
 
 	public static void main(String[] args) {
